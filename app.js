caf79ebfdf4379d4c0f9d187d6830b0cfd0f9cdd
rutik wankhade <rutikwankhade2@gmail.com>
2020-07-28 11:52:17 +0530

    added functionality

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
-
 const input = document.querySelector('.get-note');
 const addNoteBtn = document.querySelector('#add-btn');
 addNoteBtn.addEventListener("click", addNewNote);
+const allNotes = [];
 
 const color = document.querySelector('.get-color');
 const notesList = document.querySelector('.notes-list');
-const allNotes = [];
+
 
 function addNewNote() {
 
@@ -16,31 +16,44 @@ function addNewNote() {
 
     };
 
-    console.log(newNote);
+    // console.log(newNote);
 
     input.value = "";
 
     allNotes.push(newNote);
     displayNotes(allNotes);
-
-
 }
 
 
 function displayNotes(notes) {
-    notesList.innerHTML = "";
+    notesList.innerHTML = " ";
     notes.forEach(element => {
        let noteHTML= `
         <div class="note" style="background-color:${element.noteColor};">
-${element.note}
-<div>
-<img src="icons/bin.png" class="bin-icon">
-</div>
+            <div class="note-view">
+                ${element.note}
+            </div>
+            <div>
+                <a class="deleteBtn"><img src="icons/bin.png" class="bin-icon"></a>
+            </div>
         </div>
-        
         `;
 
-        notesList.insertAdjacentHTML('afterbegin',noteHTML);
+        notesList.insertAdjacentHTML('afterbegin', noteHTML);
+        
+        const deleteBtn = document.querySelector('.deleteBtn');
+        deleteBtn.addEventListener('click', e => { deleteNote(e,element);})
+
 
     });
+}
+
+
+function deleteNote(e,element) {
+    let item = e.target.parentElement.parentElement.parentElement;
+    // console.log(item);
+    item.parentNode.removeChild(item);
+    // console.log(element)
+    allNotes.pop(element);
+    
 }
